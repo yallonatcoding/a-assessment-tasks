483f8e4f182e61fa230615ff48f8f21c94388cee
yallonatcoding <jaredgilbon@gmail.com>
2025-08-11 22:27:55 -0600

    fix: add https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
